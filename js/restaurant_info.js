e7df373295411f49f2c969dc4938bef54b24f4e3
Caleb Dunn <finish.06@gmail.com>
2019-08-20 09:55:11 -0500

    add map api key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZmluaXNoMDYiLCJhIjoiY2p6anVrMTZsMGR4aDNjcHBoc2w5dWxucCJ9.Vk6oWga5GtspYS3Nq3qo4g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
